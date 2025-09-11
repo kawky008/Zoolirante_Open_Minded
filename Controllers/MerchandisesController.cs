9f6574f861e949c0b51b71b6110275855c06bbaf
nhandang1304 <danty035@mymail.unisa.edu.au>
2025-09-11 12:39:31 +0930

    Scaffold for Events and Merchandise

---
+++ Controllers/MerchandisesController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Zoolirante_Open_Minded.Models;
+
+namespace Zoolirante_Open_Minded.Controllers
+{
+    public class MerchandisesController : Controller
+    {
+        private readonly ZooliranteDatabaseContext _context;
+
+        public MerchandisesController(ZooliranteDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Merchandises
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Merchandises.ToListAsync());
+        }
+
+        // GET: Merchandises/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var merchandise = await _context.Merchandises
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (merchandise == null)
+            {
+                return NotFound();
+            }
+
+            return View(merchandise);
+        }
+
+        // GET: Merchandises/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Merchandises/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductId,Name,Description,Price,Stock,ImageUrl,Category")] Merchandise merchandise)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(merchandise);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(merchandise);
+        }
+
+        // GET: Merchandises/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var merchandise = await _context.Merchandises.FindAsync(id);
+            if (merchandise == null)
+            {
+                return NotFound();
+            }
+            return View(merchandise);
+        }
+
+        // POST: Merchandises/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,Name,Description,Price,Stock,ImageUrl,Category")] Merchandise merchandise)
+        {
+            if (id != merchandise.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(merchandise);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MerchandiseExists(merchandise.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(merchandise);
+        }
+
+        // GET: Merchandises/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var merchandise = await _context.Merchandises
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (merchandise == null)
+            {
+                return NotFound();
+            }
+
+            return View(merchandise);
+        }
+
+        // POST: Merchandises/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var merchandise = await _context.Merchandises.FindAsync(id);
+            if (merchandise != null)
+            {
+                _context.Merchandises.Remove(merchandise);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MerchandiseExists(int id)
+        {
+            return _context.Merchandises.Any(e => e.ProductId == id);
+        }
+    }
+}
