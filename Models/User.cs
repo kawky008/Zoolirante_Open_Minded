57b892ca41e4b54006065bd5952f45144b12912e
nhandang1304 <danty035@mymail.unisa.edu.au>
2025-09-15 16:10:25 +0930

    Add user account

--- Models/User.cs
+++ Models/User.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;    
 namespace Zoolirante_Open_Minded.Models;
 
 public partial class User
 {
     public int UserId { get; set; }
 
+    [Required]
+    [Display(Name ="Full Name")]
+    [StringLength(200,ErrorMessage  = "Full Name cannot exceed 200 characters.")]
     public string FullName { get; set; } = null!;
 
+    [Required]
+    [Display(Name ="Email")]
+    [DataType(DataType.EmailAddress)]
     public string Email { get; set; } = null!;
 
+    [Required]
+    [Display(Name ="Password")]
+    [DataType(DataType.Password)]
     public string PasswordHash { get; set; } = null!;
 
     public string Role { get; set; } = null!;
