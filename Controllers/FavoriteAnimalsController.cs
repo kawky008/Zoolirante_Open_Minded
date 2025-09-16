0cabd3ff7073115e4c67ce94be8db1e0f04ea93b
kawky008 <kawky008@mymail.unisa.edu.au>
2025-09-16 19:22:39 +0930

    Scaffolded FavoriteAnimals

---
+++ Controllers/FavoriteAnimalsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Zoolirante_Open_Minded.Models;
+
+namespace Zoolirante_Open_Minded.Controllers
+{
+    public class FavoriteAnimalsController : Controller
+    {
+        private readonly ZooliranteDatabaseContext _context;
+
+        public FavoriteAnimalsController(ZooliranteDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: FavoriteAnimals
+        public async Task<IActionResult> Index()
+        {
+            // Get userId
+            //var userId =
+
+            // Get fav animals
+            //var favoriteAnimals = await _context.[AssociationTableForFavoriteAnimals]
+            //    .Where(f => f.UserId == userId)
+            //    .Select(f => f.Animal)
+            //    .ToListAsync();
+
+            //return View(favoriteAnimals);
+
+            return View(await _context.Animals.ToListAsync());
+        }
+
+        // GET: FavoriteAnimals/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var animal = await _context.Animals
+                .FirstOrDefaultAsync(m => m.AnimalId == id);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
+            return View(animal);
+        }
+
+        // GET: FavoriteAnimals/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FavoriteAnimals/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AnimalId,Name,Species,Region,ConservationStatus,Habitat,Description,ImageUrl,ExhibitLocation")] Animal animal)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(animal);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(animal);
+        }
+
+        // GET: FavoriteAnimals/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var animal = await _context.Animals.FindAsync(id);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+            return View(animal);
+        }
+
+        // POST: FavoriteAnimals/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AnimalId,Name,Species,Region,ConservationStatus,Habitat,Description,ImageUrl,ExhibitLocation")] Animal animal)
+        {
+            if (id != animal.AnimalId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(animal);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AnimalExists(animal.AnimalId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(animal);
+        }
+
+        // GET: FavoriteAnimals/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var animal = await _context.Animals
+                .FirstOrDefaultAsync(m => m.AnimalId == id);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
+            return View(animal);
+        }
+
+        // POST: FavoriteAnimals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var animal = await _context.Animals.FindAsync(id);
+            if (animal != null)
+            {
+                _context.Animals.Remove(animal);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AnimalExists(int id)
+        {
+            return _context.Animals.Any(e => e.AnimalId == id);
+        }
+    }
+}
