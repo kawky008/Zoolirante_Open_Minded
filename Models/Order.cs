8df4a9db0d09e8c0a8b146042c4e4c5a6993bb22
nhandang1304 <danty035@mymail.unisa.edu.au>
2025-10-02 22:19:17 +0930

    add Items in Order Model and add logic to save order

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Zoolirante_Open_Minded.Models;
 
@@ -14,16 +15,18 @@ public partial class Order
     public DateTime OrderDate { get; set; }
 
     public decimal? TotalAmount { get; set; }
+    public string Items { get; set; }
 
-    public string Status { get; set; } = null!;
+    public string Status { get; set; } = null;
 
-    public DateOnly? PickupDate { get; set; }
+    [Required]
+    [Display(Name = "Date and Time Pickup")]
+    public DateTime PickupDate { get; set; }
 
-    public string? PickupWindow { get; set; }
 
     public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
 
-    public virtual PickupLocation PickupLocation { get; set; } = null!;
+    public virtual PickupLocation PickupLocation { get; set; } = null;
 
-    public virtual User User { get; set; } = null!;
+    public virtual User User { get; set; } = null;
 }
