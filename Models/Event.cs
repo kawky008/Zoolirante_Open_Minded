0a5586785506f328853a96c07e7b984a108537a1
Tingwei xie <xiett001@mymail.unisa.edu.au>
2025-10-09 16:39:59 +1030

    Additional decorations added

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+
 
 namespace Zoolirante_Open_Minded.Models;
 
@@ -7,20 +11,31 @@ public partial class Event
 {
     public int EventId { get; set; }
 
+    [Required, StringLength(150)]
     public string Title { get; set; } = null!;
 
     public string? Description { get; set; }
 
+    [DataType(DataType.DateTime)]
     public DateTime StartTime { get; set; }
 
+    [DataType(DataType.DateTime)]
     public DateTime EndTime { get; set; }
 
+    [Range(1, int.MaxValue, ErrorMessage = "Capacity must be at least 1")]
     public int? Capacity { get; set; }
 
+    [Range(0, 999999.99, ErrorMessage = "Price cannot be negative")]
     public decimal Price { get; set; }
 
+    [Required, StringLength(100)]
     public string Location { get; set; } = null!;
-    public string ImageUrl { get; set; } = null!;
+
+    [ValidateNever]               
+    public string? ImageUrl { get; set; }   
+
 
     public virtual ICollection<Animal> Animals { get; set; } = new List<Animal>();
 }
+
+
